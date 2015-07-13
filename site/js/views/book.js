4070457b26907d13fbf618e6b6e21fdf84e43a3f
Kimbsy <dave.kimber@simitive.com>
2015-07-13 12:50:17 +0100

    basic app up and runing

---
+++ site/js/views/book.js
@@ -0,0 +1,16 @@
+// view for displaying a single book
+
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+  tagName: 'div',
+  className: 'bookContainer',
+  template: _.template($('#bookTemplate').html()),
+
+  render: function() {
+    // this.$el is what we defined in tagName. use $el to get access to jquery html() function
+    this.$el.html(this.template(this.model.attributes));
+
+    return this;
+  }
+});
